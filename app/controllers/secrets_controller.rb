2a645f6d684d698186ba2389546a6e61033918fb
Shawn Ren <shawnren527@gmail.com>
2019-05-05 22:28:05 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,9 +1,6 @@
 class SecretsController < SessionsController
   before_action :require_login
   def show
-    if session.include? :name == false
-      redirect_to login_path
-    end
   end
 
   private
