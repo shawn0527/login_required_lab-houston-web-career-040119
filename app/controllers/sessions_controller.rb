465366ee892f6aae0454ffa818f17b845877478d
Shawn Ren <shawnren527@gmail.com>
2019-05-05 22:14:58 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,13 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def show    
+  def show
   end
 
   def create
     current_user = params[:name]
     if current_user == nil || current_user.empty?
-      render :new
+      redirect_to login_path
     else
       render :show
     end
