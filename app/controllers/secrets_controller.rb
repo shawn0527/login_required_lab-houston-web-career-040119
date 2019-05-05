d4c6465f32d384e2977148b3c6274fa49b6af660
Shawn Ren <shawnren527@gmail.com>
2019-05-05 22:24:21 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,7 +1,7 @@
 class SecretsController < SessionsController
   before_action :require_login
   def show
-    if session[:name] == nil || sesion[:name].empty?
+    if session[:name] == nil || session[:name].empty?
       redirect_to login_path
     end
   end
